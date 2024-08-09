bf8626a12d3828cd3e8cbdd84b94aaef8e7c2a7c
HudsonWayne <waynewaynebenhura@gmail.com>
2024-08-09 09:56:08 +0200

    adding a colors front and back

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyCSharpApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+        }
+    }
+}
